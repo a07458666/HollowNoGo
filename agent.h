4b09937eb6e393750e1ee28603cb3dc72f401a1f
AndySu <a07458666@gmail.com>
2021-11-27 14:08:24 +0800

    [add]MCTS V1 [TODO]First player error

--- agent.h
+++ agent.h
@@ -17,35 +17,56 @@
 #include "board.h"
 #include "action.h"
 #include <fstream>
+#include <math.h>
 
-class agent {
+enum PloyType
+{
+	randomPloy,
+	mctsPloy
+};
+
+struct Node
+{
+	int nb;
+	int value;
+	std::vector<Node *> childNodes;
+	action::place selectPlace;
+};
+
+#define FLT_MIN 1.175494351e-38
+
+class agent
+{
 public:
-	agent(const std::string& args = "") {
+	agent(const std::string &args = "")
+	{
 		std::stringstream ss("name=unknown role=unknown " + args);
-		for (std::string pair; ss >> pair; ) {
+		for (std::string pair; ss >> pair;)
+		{
 			std::string key = pair.substr(0, pair.find('='));
 			std::string value = pair.substr(pair.find('=') + 1);
-			meta[key] = { value };
+			meta[key] = {value};
 		}
 	}
 	virtual ~agent() {}
-	virtual void open_episode(const std::string& flag = "") {}
-	virtual void close_episode(const std::string& flag = "") {}
-	virtual action take_action(const board& b) { return action(); }
-	virtual bool check_for_win(const board& b) { return false; }
+	virtual void open_episode(const std::string &flag = "") {}
+	virtual void close_episode(const std::string &flag = "") {}
+	virtual action take_action(const board &b) { return action(); }
+	virtual bool check_for_win(const board &b) { return false; }
 
 public:
-	virtual std::string property(const std::string& key) const { return meta.at(key); }
-	virtual void notify(const std::string& msg) { meta[msg.substr(0, msg.find('='))] = { msg.substr(msg.find('=') + 1) }; }
+	virtual std::string property(const std::string &key) const { return meta.at(key); }
+	virtual void notify(const std::string &msg) { meta[msg.substr(0, msg.find('='))] = {msg.substr(msg.find('=') + 1)}; }
 	virtual std::string name() const { return property("name"); }
 	virtual std::string role() const { return property("role"); }
 
 protected:
 	typedef std::string key;
-	struct value {
+	struct value
+	{
 		std::string value;
 		operator std::string() const { return value; }
-		template<typename numeric, typename = typename std::enable_if<std::is_arithmetic<numeric>::value, numeric>::type>
+		template <typename numeric, typename = typename std::enable_if<std::is_arithmetic<numeric>::value, numeric>::type>
 		operator numeric() const { return numeric(std::stod(value)); }
 	};
 	std::map<key, value> meta;
@@ -54,9 +75,11 @@ protected:
 /**
  * base agent for agents with randomness
  */
-class random_agent : public agent {
+class random_agent : public agent
+{
 public:
-	random_agent(const std::string& args = "") : agent(args) {
+	random_agent(const std::string &args = "") : agent(args)
+	{
 		if (meta.find("seed") != meta.end())
 			engine.seed(int(meta["seed"]));
 	}
@@ -70,26 +93,36 @@ protected:
  * random player for both side
  * put a legal piece randomly
  */
-class player : public random_agent {
+class player : public random_agent
+{
 public:
-	player(const std::string& args = "") : random_agent("name=random role=unknown " + args),
-		space(board::size_x * board::size_y), who(board::empty) {
+	player(const std::string &args = "") : random_agent("name=random role=unknown " + args),
+										   space(board::size_x * board::size_y), who(board::empty)
+	{
 		if (name().find_first_of("[]():; ") != std::string::npos)
 			throw std::invalid_argument("invalid name: " + name());
-		if (role() == "black") who = board::black;
-		if (role() == "white") who = board::white;
+		if (role() == "black")
+			who = board::black;
+		if (role() == "white")
+			who = board::white;
 		if (who == board::empty)
 			throw std::invalid_argument("invalid role: " + role());
 		for (size_t i = 0; i < space.size(); i++)
 			space[i] = action::place(i, who);
 	}
 
-	virtual action take_action(const board& state) {
-		std::shuffle(space.begin(), space.end(), engine);
-		for (const action::place& move : space) {
-			board after = state;
-			if (move.apply(after) == board::legal)
-				return move;
+	virtual action take_action(const board &state)
+	{
+		switch (ploy())
+		{
+		case PloyType::randomPloy:
+			return random_action(state);
+			break;
+		case PloyType::mctsPloy:
+			return mcts_action(state);
+			break;
+		default:
+			break;
 		}
 		return action();
 	}
@@ -97,4 +130,234 @@ public:
 private:
 	std::vector<action::place> space;
 	board::piece_type who;
+	PloyType ploy() const
+	{
+		if (property("ploy") == "mcts")
+			return PloyType::mctsPloy;
+		else
+			return PloyType::randomPloy;
+	}
+
+	action random_action(const board &state)
+	{
+		// create_space(state, who, space);
+		// std::cout << who << std::endl;
+		std::shuffle(space.begin(), space.end(), engine);
+		for (const action::place &move : space)
+		{
+			board after = state;
+			if (move.apply(after) == board::legal)
+				return move;
+		}
+		return action();
+	}
+
+	action mcts_action(const board &state)
+	{
+		Node root = {0, 0, {}, action::place()};
+		create_node_leaf(state, who, &root);
+		int times_count = 0;
+		while (times_count < 500)
+		{
+			playOneSequence(state, &root);
+			times_count++;
+		}
+		int maxIndex = -1;
+		int maxnb = 0;
+		// std::cout << "===========START===========" << std::endl;
+		// std::cout << state << std::endl;
+		for (int i = 0; i < root.childNodes.size(); i++)
+		{
+			if (root.childNodes[i]->nb > maxnb)
+			{
+				// board after = state;
+				// root.childNodes[i]->selectPlace.apply(after);
+				// std::cout << "nb " << root.childNodes[i]->nb << std::endl;
+				// std::cout << "(x,y)" << root.childNodes[i]->selectPlace.position().x << "," << root.childNodes[i]->selectPlace.position().y << std::endl;
+				// std::cout << after << std::endl;
+				maxnb = root.childNodes[i]->nb;
+				maxIndex = i;
+			}
+		}
+		if (maxIndex != -1)
+			return root.childNodes[maxIndex]->selectPlace;
+		return action();
+	}
+
+	// One Simulation
+	void playOneSequence(const board &state, Node *rootNode)
+	{
+		std::vector<Node *> nodePath;
+		nodePath.emplace_back(rootNode);
+		int index = 0;
+		board after = state;
+		board::piece_type currentWho = who;
+		while (nodePath.back()->childNodes.size() != 0)
+		{
+			nodePath.emplace_back(descendByUCB1(after, nodePath.back()));
+			nodePath.back()->selectPlace.apply(after);
+			index++;
+			if (currentWho == board::black)
+				currentWho = board::white;
+			else
+				currentWho = board::black;
+		}
+		play_game_by_policy(after, currentWho, nodePath.back());
+		create_node_leaf(after, currentWho, nodePath.back());
+		updateValue(nodePath, -nodePath.back()->value);
+	}
+	// Selection
+	Node *descendByUCB1(const board &state, Node *node)
+	{
+		float nb = 0;
+		for (Node *currentNode : node->childNodes)
+		{
+			nb += currentNode->nb;
+		}
+
+		float max_v = FLT_MIN;
+		float max_index = 0;
+		for (int i = 0; i < node->childNodes.size(); i++)
+		{
+			if (node->childNodes[i]->nb == 0)
+			{
+				max_v = INT_MAX;
+				max_index = i;
+				break;
+			}
+			else
+			{
+				float v = (-node->childNodes[i]->value / node->childNodes[i]->nb) + (sqrt(2 * log(nb) / node->childNodes[i]->nb));
+				if (v > max_v)
+				{
+					max_v = v;
+					max_index = i;
+				}
+			}
+		}
+		// std::cout << "max_index " << max_index << std::endl;
+		// std::cout << "node->childNodes.size() " << node->childNodes.size() << std::endl;
+		return node->childNodes[max_index];
+	}
+
+	// back propagation
+	void updateValue(std::vector<Node *> nodePath, float v)
+	{
+		float locvalue = v;
+		for (int i = 0; i < nodePath.size() - 1; i++)
+		{
+			nodePath[i]->nb += 1;
+			nodePath[i]->value += locvalue;
+			locvalue = -locvalue;
+		}
+	}
+
+	//
+	void create_node_leaf(const board &state, board::piece_type whoRound, Node *node)
+	{
+		// std::vector<action::place> spaceRound(board::size_x * board::size_y);
+		// for (size_t i = 0; i < spaceRound.size(); i++)
+		// 	spaceRound[i] = action::place(i, whoRound);
+		std::vector<action::place> spaceRound; //(board::size_x * board::size_y);
+		create_space(state, whoRound, spaceRound);
+		auto newSize = std::min((size_t)20, spaceRound.size());
+		spaceRound.resize(newSize);
+		// std::shuffle(spaceRound.begin(), spaceRound.end(), engine);
+		// std::cout << "==========BEGIN===========" << std::endl;
+		// std::cout << "whoRound" << whoRound << std::endl;
+		// std::cout << state << std::endl;
+		// std::cout << "==========ALL IN===========" << std::endl;
+		for (const action::place &move : spaceRound)
+		{
+			board after = state;
+			if (move.apply(after) == board::legal)
+			{
+				// std::cout << "(x,y)" << move.position().x << "," << move.position().y << std::endl;
+				node->childNodes.emplace_back(new Node{0, 0, {}, move});
+				// std::cout << after << std::endl;
+			}
+		}
+		// std::cout << "==========END===========" << std::endl;
+	}
+
+	//
+	void play_game_by_policy(const board &state, board::piece_type whoFirst, Node *node)
+	{
+		board::piece_type whoWin = play(state, whoFirst);
+		if (whoWin == board::black)
+			node->value = 1;
+		else
+			node->value = 0;
+		node->nb = 1;
+	}
+
+	board::piece_type play(const board &state, board::piece_type whoFirst)
+	{
+		board::piece_type whoRound = whoFirst;
+		board after = state;
+		action::place move;
+		do
+		{
+			move = playOneHand(after, whoRound);
+			move.apply(after);
+			if (whoRound == board::black)
+				whoRound = board::white;
+			else
+				whoRound = board::black;
+		} while (move != -1u);
+		return whoRound;
+	}
+
+	action playOneHand(const board &state, board::piece_type whoFirst)
+	{
+		std::vector<action::place> spaceRound;
+		create_space(state, whoFirst, spaceRound);
+		for (const action::place &move : spaceRound)
+		{
+			board after = state;
+			if (move.apply(after) == board::legal)
+			{
+				// std::cout << after << std::endl;
+				return move;
+			}
+		}
+		return action();
+	}
+
+	void create_space(const board &state, board::piece_type whoFirst, std::vector<action::place> &spaceSort)
+	{
+		std::vector<action::place> best;
+		std::vector<action::place> normal;
+		std::vector<action::place> bad;
+		for (int x = 0; x < board::size_x; x++)
+		{
+			for (int y = 0; y < board::size_y; y++)
+			{
+				board after = state;
+				if (action::place(x, y, whoFirst).apply(after) == board::legal)
+				{
+					int liberty = 0;
+					if (x < board::size_x - 1 && after[x + 1][y] == board::piece_type::empty)
+						liberty++;
+					if (x > 0 && after[x - 1][y] == board::piece_type::empty)
+						liberty++;
+					if (y < board::size_y - 1 && after[x][y + 1] == board::piece_type::empty)
+						liberty++;
+					if (y > 0 && after[x][y - 1] == board::piece_type::empty)
+						liberty++;
+					if (liberty == 4)
+						spaceSort.emplace_back(action::place(x, y, whoFirst));
+					else if (liberty == 3)
+						best.emplace_back(action::place(x, y, whoFirst));
+					else if (liberty == 2)
+						normal.emplace_back(action::place(x, y, whoFirst));
+					else
+						bad.emplace_back(action::place(x, y, whoFirst));
+				}
+			}
+		}
+		spaceSort.insert(spaceSort.end(), best.begin(), best.end());
+		spaceSort.insert(spaceSort.end(), normal.begin(), normal.end());
+		spaceSort.insert(spaceSort.end(), bad.begin(), bad.end());
+	}
 };
--- nogo.cpp
+++ nogo.cpp
@@ -17,109 +17,156 @@
 #include "episode.h"
 #include "statistic.h"
 
-int main(int argc, const char* argv[]) {
+int main(int argc, const char *argv[])
+{
 	std::cout << "HollowNoGo-Demo: ";
-	std::copy(argv, argv + argc, std::ostream_iterator<const char*>(std::cout, " "));
-	std::cout << std::endl << std::endl;
+	std::copy(argv, argv + argc, std::ostream_iterator<const char *>(std::cout, " "));
+	std::cout << std::endl
+			  << std::endl;
 
 	size_t total = 1000, block = 0, limit = 0;
 	std::string black_args, white_args;
 	std::string load, save;
 	std::string name = "TCG-HollowNoGo-Demo", version = "2021"; // for GTP shell
 	bool summary = false, shell = false;
-	for (int i = 1; i < argc; i++) {
+	for (int i = 1; i < argc; i++)
+	{
 		std::string para(argv[i]);
-		if (para.find("--total=") == 0) {
+		if (para.find("--total=") == 0)
+		{
 			total = std::stoull(para.substr(para.find("=") + 1));
-		} else if (para.find("--block=") == 0) {
+		}
+		else if (para.find("--block=") == 0)
+		{
 			block = std::stoull(para.substr(para.find("=") + 1));
-		} else if (para.find("--limit=") == 0) {
+		}
+		else if (para.find("--limit=") == 0)
+		{
 			limit = std::stoull(para.substr(para.find("=") + 1));
-		} else if (para.find("--black=") == 0) {
+		}
+		else if (para.find("--black=") == 0)
+		{
 			black_args = para.substr(para.find("=") + 1);
-		} else if (para.find("--white=") == 0) {
+		}
+		else if (para.find("--white=") == 0)
+		{
 			white_args = para.substr(para.find("=") + 1);
-		} else if (para.find("--load=") == 0) {
+		}
+		else if (para.find("--load=") == 0)
+		{
 			load = para.substr(para.find("=") + 1);
-		} else if (para.find("--save=") == 0) {
+		}
+		else if (para.find("--save=") == 0)
+		{
 			save = para.substr(para.find("=") + 1);
-		} else if (para.find("--name=") == 0) {
+		}
+		else if (para.find("--name=") == 0)
+		{
 			name = para.substr(para.find("=") + 1);
-		} else if (para.find("--version=") == 0) {
+		}
+		else if (para.find("--version=") == 0)
+		{
 			version = para.substr(para.find("=") + 1);
-		} else if (para.find("--summary") == 0) {
+		}
+		else if (para.find("--summary") == 0)
+		{
 			summary = true;
-		} else if (para.find("--shell") == 0) {
+		}
+		else if (para.find("--shell") == 0)
+		{
 			shell = true;
 		}
 	}
 
 	statistic stat(total, block, limit);
 
-	if (load.size()) {
+	if (load.size())
+	{
 		std::ifstream in(load, std::ios::in);
 		in >> stat;
 		in.close();
 		summary |= stat.is_finished();
 	}
 
-	player black("name=black " + black_args + " role=black");
-	player white("name=white " + white_args + " role=white");
+	player black("name=black ploy=random " + black_args + " role=black");
+	player white("name=white ploy=random " + white_args + " role=white");
 
-	if (!shell) { // launch standard local games
-		while (!stat.is_finished()) {
+	if (!shell)
+	{ // launch standard local games
+		while (!stat.is_finished())
+		{
 			black.open_episode("~:" + white.name());
 			white.open_episode(black.name() + ":~");
 
 			stat.open_episode(black.name() + ":" + white.name());
-			episode& game = stat.back();
-			while (true) {
-				agent& who = game.take_turns(black, white);
+			episode &game = stat.back();
+			while (true)
+			{
+				agent &who = game.take_turns(black, white);
 				action move = who.take_action(game.state());
-				if (game.apply_action(move) != true) break;
-				if (who.check_for_win(game.state())) break;
+				std::cout << game.state() << std::endl;
+				if (game.apply_action(move) != true)
+					break;
+				if (who.check_for_win(game.state()))
+					break;
 			}
-			agent& win = game.last_turns(black, white);
+			agent &win = game.last_turns(black, white);
 			stat.close_episode(win.name());
 
 			black.close_episode(win.name());
 			white.close_episode(win.name());
 		}
-	} else { // launch GTP shell
-		for (std::string command; std::getline(std::cin, command); ) {
-			if (command.back() == '\r') command.pop_back();
-			if (command.empty()) continue;
+	}
+	else
+	{ // launch GTP shell
+		for (std::string command; std::getline(std::cin, command);)
+		{
+			if (command.back() == '\r')
+				command.pop_back();
+			if (command.empty())
+				continue;
 
 			std::vector<std::string> args;
 			std::istringstream iss(command);
-			for (std::string s; getline(iss, s, ' '); args.push_back(s));
+			for (std::string s; getline(iss, s, ' '); args.push_back(s))
+				;
 
 			std::string reply;
-			if (args[0] == "play" || args[0] == "genmove") { // play a move, or generate a move and play
-				if (!stat.is_episode_ongoing()) { // should open an episode
+			if (args[0] == "play" || args[0] == "genmove")
+			{ // play a move, or generate a move and play
+				if (!stat.is_episode_ongoing())
+				{ // should open an episode
 					black.open_episode("~:" + white.name());
 					white.open_episode(black.name() + ":~");
 					stat.open_episode(black.name() + ":" + white.name());
 				}
 
-				episode& game = stat.back();
-				agent& who = game.take_turns(black, white);
-				if (who.role()[0] != std::tolower(args[1][0])) { // player mismatch?!
-					std::cout << "= " << "resign" << std::endl << std::endl;
+				episode &game = stat.back();
+				agent &who = game.take_turns(black, white);
+				if (who.role()[0] != std::tolower(args[1][0]))
+				{ // player mismatch?!
+					std::cout << "= "
+							  << "resign" << std::endl
+							  << std::endl;
 					// show the error message and terminate the shell
 					std::cerr << "player color " << args[1] << " mismatch!" << std::endl;
 					std::cerr << "current state, "
-					          << who.role() << " to play: " << std::endl << game.state();
+							  << who.role() << " to play: " << std::endl
+							  << game.state();
 					break;
 				}
-				if (args[0] == "play") { // play a move
+				if (args[0] == "play")
+				{							   // play a move
 					std::string types = "?bw"; // black == 1, white == 2
 					action::place move(args[2], types.find(who.role()[0]));
-					if (game.apply_action(move) != true) { // remote plays an illegal move?!
-						std::cout << "= " << "resign" << std::endl << std::endl;
+					if (game.apply_action(move) != true)
+					{ // remote plays an illegal move?!
+						std::cout << "= "
+								  << "resign" << std::endl
+								  << std::endl;
 						// show the error message and terminate the shell
 						std::cerr << who.role() << " plays an illegal action!" << std::endl;
-						const char* reason[] = {
+						const char *reason[] = {
 							"legal",
 							"illegal_turn",
 							"illegal_pass",
@@ -129,65 +176,98 @@ int main(int argc, const char* argv[]) {
 							"illegal_take",
 							"unknown",
 						};
-						std::cerr << "current state: " << std::endl << game.state();
+						std::cerr << "current state: " << std::endl
+								  << game.state();
 						int code = move.apply(game.state());
 						std::cerr << "action: " << args[1] << " " << args[2] << std::endl;
 						std::cerr << "reason: " << reason[std::min(-code, 7)] << std::endl;
 						break;
 					}
-				} else if (args[0] == "genmove") { // generate a move and play
+				}
+				else if (args[0] == "genmove")
+				{ // generate a move and play
 					action::place move = who.take_action(game.state());
-					if (game.apply_action(move) == true) {
+					if (game.apply_action(move) == true)
+					{
 						reply = move.position();
-					} else { // I have no legal move to play
+					}
+					else
+					{ // I have no legal move to play
 						reply = "resign";
 					}
 				}
-
-			} else if (args[0] == "clear_board" || args[0] == "quit") { // reset game, or quit
-				if (stat.is_episode_ongoing()) { // should close an opened episode
-					agent& win = stat.back().last_turns(black, white);
+			}
+			else if (args[0] == "clear_board" || args[0] == "quit")
+			{ // reset game, or quit
+				if (stat.is_episode_ongoing())
+				{ // should close an opened episode
+					agent &win = stat.back().last_turns(black, white);
 					stat.close_episode(win.name());
 					black.close_episode(win.name());
 					white.close_episode(win.name());
 				}
-				if (args[0] == "quit") break; // quit GTP shell
-
-			} else if (args[0] == "showboard") { // print the board
+				if (args[0] == "quit")
+					break; // quit GTP shell
+			}
+			else if (args[0] == "showboard")
+			{ // print the board
 				std::stringstream buf;
 				buf << (stat.is_episode_ongoing() ? stat.back().state() : board());
 				reply = "\n" + buf.str();
 				reply.pop_back(); // remove a new line
-
-			} else if (args[0] == "boardsize") { // set the board size
+			}
+			else if (args[0] == "boardsize")
+			{ // set the board size
 				size_t size = std::stoul(args[1]);
-				if (size != board::size_x || size != board::size_y) {
+				if (size != board::size_x || size != board::size_y)
+				{
 					std::cerr << "board size mismatch: " << args[1] << std::endl;
 				}
-				if (size > board::size_x || size > board::size_y) break;
-
-			} else if (args[0] == "name") { // report the name of the program
+				if (size > board::size_x || size > board::size_y)
+					break;
+			}
+			else if (args[0] == "name")
+			{ // report the name of the program
 				reply = name;
-			} else if (args[0] == "version") { // report the version number of the program
+			}
+			else if (args[0] == "version")
+			{ // report the version number of the program
 				reply = version;
-			} else if (args[0] == "protocol_version") { // report GTP protocol version
+			}
+			else if (args[0] == "protocol_version")
+			{ // report GTP protocol version
 				reply = "2";
-			} else if (args[0] == "list_commands") { // print supported commands
-				reply = "play\n" "genmove\n" "clear_board\n" "showboard\n" "boardsize\n"
-				        "name\n" "version\n" "protocol_version\n" "list_commands\n" "quit\n";
-			} else {
+			}
+			else if (args[0] == "list_commands")
+			{ // print supported commands
+				reply = "play\n"
+						"genmove\n"
+						"clear_board\n"
+						"showboard\n"
+						"boardsize\n"
+						"name\n"
+						"version\n"
+						"protocol_version\n"
+						"list_commands\n"
+						"quit\n";
+			}
+			else
+			{
 				reply = "unknown command";
 			}
 
-			std::cout << "= " << reply << std::endl << std::endl;
+			std::cout << "= " << reply << std::endl
+					  << std::endl;
 		}
 	}
 
-	if (summary) {
+	if (summary)
+	{
 		stat.summary();
 	}
 
-	if (save.size()) {
+	if (save.size())
+	{
 		std::ofstream out(save, std::ios::out | std::ios::trunc);
 		out << stat;
 		out.close();
